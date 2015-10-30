b421ee46fa72f43eec15bfb395fe1a1562102056
kpl-grwl <kapil.agrawal85@gmail.com>
2015-10-30 02:22:44 -0400

    updated query to include cwd

--- include/runDetail.php
+++ include/runDetail.php
@@ -8,13 +8,14 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql="SELECT xr.run_id AS RunId, xr.job_id AS JobId, xr.date AS Date,
+    $sql="
+        SELECT xr.run_id AS RunId, xr.job_id AS JobId, xr.date AS Date,
         CONCAT(xr.num_cores,'     ',  xr.job_num_cores, '     ',num_nodes, '    ', num_threads) AS
         'Cores JobNumCores  Nodes Threads', 
         xr.account AS Account, xr.exec_type AS ExecType, xr.run_time AS RunTime,
-        xr.queue as Queue,
         xr.exit_code AS ExitCode,
-       xr.user AS RunUser 
+        xr.user AS RunUser, 
+        xr.cwd AS Cwd 
         FROM xalt_run AS xr
         WHERE 
         xr.uuid = '$uuid' 
@@ -33,9 +34,9 @@ try {
 {\"id\":\"\",\"label\":\"Account\",\"pattern\":\"\",\"type\":\"string\"}, 
 {\"id\":\"\",\"label\":\"Exec Type\",\"pattern\":\"\",\"type\":\"string\"}, 
 {\"id\":\"\",\"label\":\"Run Time (sec)\",\"pattern\":\"\",\"type\":\"string\"}, 
-{\"id\":\"\",\"label\":\"Queue\",\"pattern\":\"\",\"type\":\"string\"}, 
 {\"id\":\"\",\"label\":\"ExitCode\",\"pattern\":\"\",\"type\":\"string\"}, 
-{\"id\":\"\",\"label\":\"Run User\",\"pattern\":\"\",\"type\":\"string\"} 
+{\"id\":\"\",\"label\":\"Run User\",\"pattern\":\"\",\"type\":\"string\"}, 
+{\"id\":\"\",\"label\":\"CurrentWorkingDir\",\"pattern\":\"\",\"type\":\"string\"} 
 ], 
 \"rows\": [ ";
 
@@ -54,9 +55,9 @@ foreach($result as $row){
     {\"v\":\"" . $row['Account'] . "\",\"f\":null},
     {\"v\":\"" . $row['ExecType'] . "\",\"f\":null},
     {\"v\":\"" . $row['RunTime'] . "\",\"f\":null},
-    {\"v\":\"" . $row['Queue'] . "\",\"f\":null},
     {\"v\":\"" . $row['ExitCode'] . "\",\"f\":null},
-    {\"v\":\"" . $row['RunUser'] . "\",\"f\":null}
+    {\"v\":\"" . $row['RunUser'] . "\",\"f\":null},
+    {\"v\":\"" . $row['Cwd'] . "\",\"f\":null}
     ]}";
     } else {
         echo "{\"c\":[
@@ -67,9 +68,9 @@ foreach($result as $row){
     {\"v\":\"" . $row['Account'] . "\",\"f\":null},
     {\"v\":\"" . $row['ExecType'] . "\",\"f\":null},
     {\"v\":\"" . $row['RunTime'] . "\",\"f\":null},
-    {\"v\":\"" . $row['Queue'] . "\",\"f\":null},
     {\"v\":\"" . $row['ExitCode'] . "\",\"f\":null},
-    {\"v\":\"" . $row['RunUser'] . "\",\"f\":null}
+    {\"v\":\"" . $row['RunUser'] . "\",\"f\":null},
+    {\"v\":\"" . $row['Cwd'] . "\",\"f\":null}
     ]}, ";
     } 
 
