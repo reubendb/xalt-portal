7d805160de464cdcfd42fae7ccc146d932dfb3dc
kpl-grwl <kapil.agrawal85@gmail.com>
2015-10-30 14:15:05 -0400

    removed exec filter from query and added few new columns

--- include/compUserList.php
+++ include/compUserList.php
@@ -11,19 +11,18 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if ($linkProgram == 'gpp') { $linkProgram = 'g++'; }              # specail condition for g++ as ajax call reads + as special character
-    $sql= "SELECT xl.build_user as Users, COUNT(xl.date) as Count 
-    FROM xalt_link xl 
-    WHERE xl.link_program = '$linkProgram' AND 
-    xl.build_syshost = '$sysHost' AND
-    xl.exec_path NOT LIKE '%.so' AND -- exec filter starts  
-    xl.exec_path NOT LIKE '%.o' AND                         
-    xl.exec_path NOT LIKE '%.o.%' AND                       
-    xl.exec_path NOT LIKE '%.so.%' AND -- exec filter ends  
-    xl.date BETWEEN '$startDate' AND '$endDate'
-    GROUP BY Users 
-    ORDER BY Count desc 
-    ;";
+    if ($linkProgram == 'gpp') { $linkProgram = 'g++'; }              # specail condition for g++ 
+                                                                      # as ajax call reads + as special character
+        $sql= "
+        SELECT xl.build_user as Users, COUNT(xl.date) as Count,
+            min(xl.date) as MinDate, max(xl.date) as MaxDate
+            FROM xalt_link xl 
+            WHERE xl.link_program = '$linkProgram' AND 
+            xl.build_syshost = '$sysHost' AND
+            xl.date BETWEEN '$startDate' AND '$endDate'
+            GROUP BY Users 
+            ORDER BY Count desc 
+            ;";
 
     $query = $conn->prepare($sql);
     $query->execute();
@@ -32,6 +31,8 @@ try {
 
     echo "{ \"cols\": [
     {\"id\":\"\",\"label\":\"Users\",\"pattern\":\"\",\"type\":\"string\"}, 
+    {\"id\":\"\",\"label\":\"Earliest_LinkDate\",\"pattern\":\"\",\"type\":\"string\"}, 
+    {\"id\":\"\",\"label\":\"Latest_LinkDate\",\"pattern\":\"\",\"type\":\"string\"}, 
     {\"id\":\"\",\"label\":\"Count\",\"pattern\":\"\",\"type\":\"number\"} 
     ], 
     \"rows\": [ ";
@@ -45,11 +46,15 @@ try {
         if ($row_num == $total_rows){
             echo "{\"c\":[
         {\"v\":\"" . $row['Users'] . "\",\"f\":null},
+        {\"v\":\"" . $row['MinDate'] . "\",\"f\":null},
+        {\"v\":\"" . $row['MaxDate'] . "\",\"f\":null},
         {\"v\":" . $row['Count'] . ",\"f\":null}
         ]}";
         } else {
             echo "{\"c\":[
         {\"v\":\"" . $row['Users'] . "\",\"f\":null},
+        {\"v\":\"" . $row['MinDate'] . "\",\"f\":null},
+        {\"v\":\"" . $row['MaxDate'] . "\",\"f\":null},
         {\"v\":" . $row['Count'] . ",\"f\":null}
         ]}, ";
         } 
