2c70680a013f3b240c895a07a2e0ac486674713e
kpl-grwl <kapil.agrawal85@gmail.com>
2015-10-30 02:18:44 -0400

    updated query to include exec_path

--- include/execDetailList.php
+++ include/execDetailList.php
@@ -28,12 +28,13 @@ try {
         break;
     }
 
-
-
-    $sql= "SELECT DISTINCT xl.uuid as Uuid,                                
+    $sql= "
+        SELECT DISTINCT xl.uuid as Uuid,                                
         xl.date as Date,                                        
         xl.link_program as LinkProgram,                         
         xl.exit_code as ExitCode,
+        xl.build_user as BuildUser,
+        xl.exec_path as ExecPath,
         IF (
             (SELECT count(*) 
             from xalt_run xr1 
@@ -46,15 +47,10 @@ try {
             INNER JOIN xalt_object xo ON (jlo.obj_id = xo.obj_id)
             WHERE 
             xo.syshost='$sysHost' AND 
-            xo.module_name LIKE '$moduleName' AND
-            xo.timestamp BETWEEN '$startDate' AND '$endDate'
+            xo.module_name LIKE '$moduleName' 
         ) 
         ka ON ka.link_id = xl.link_id  AND
-        xl.exec_path NOT LIKE '%.so' AND -- exec filter starts                
-        xl.exec_path NOT LIKE '%.o' AND                                       
-        xl.exec_path NOT LIKE '%.o.%' AND                                     
-        xl.exec_path NOT LIKE '%.so.%' AND -- exec filter ends 
-        xl.build_user like CONCAT ('%', '$user', '%')  AND                            
+        xl.date BETWEEN '$startDate' AND '$endDate' AND
         SUBSTRING_INDEX(xl.exec_path, '/', -1) = '$exec' 
         ORDER BY Date desc
         ;";
@@ -69,11 +65,13 @@ try {
 #    print_r($result);
 
     echo "{ \"cols\": [
-    {\"id\":\"\",\"label\":\"Unique Id\",\"pattern\":\"\",\"type\":\"string\"}, 
+    {\"id\":\"\",\"label\":\"Executable Path\",\"pattern\":\"\",\"type\":\"string\"}, 
     {\"id\":\"\",\"label\":\"Build Date\",\"pattern\":\"\",\"type\":\"string\"}, 
     {\"id\":\"\",\"label\":\"Link Program\",\"pattern\":\"\",\"type\":\"string\"}, 
     {\"id\":\"\",\"label\":\"Exit Code\",\"pattern\":\"\",\"type\":\"string\"}, 
-    {\"id\":\"\",\"label\":\"Job Run[T/F]\",\"pattern\":\"\",\"type\":\"boolean\"} 
+    {\"id\":\"\",\"label\":\"Build User\",\"pattern\":\"\",\"type\":\"string\"}, 
+    {\"id\":\"\",\"label\":\"Job Run[T/F]\",\"pattern\":\"\",\"type\":\"boolean\"}, 
+    {\"id\":\"\",\"label\":\"Unique Id\",\"pattern\":\"\",\"type\":\"string\"}
     ], 
     \"rows\": [ ";
 
@@ -85,19 +83,23 @@ try {
 
         if ($row_num == $total_rows){
             echo "{\"c\":[
-        {\"v\":\"" . $row['Uuid'] . "\",\"f\":null},
+        {\"v\":\"" . $row['ExecPath'] . "\",\"f\":null},
         {\"v\":\"" . $row['Date'] . "\",\"f\":null},
         {\"v\":\"" . $row['LinkProgram'] . "\",\"f\":null},
         {\"v\":\"" . $row['ExitCode'] . "\",\"f\":null},
-        {\"v\":" . $row['JobRun'] . ",\"f\":null}
+        {\"v\":\"" . $row['BuildUser'] . "\",\"f\":null},
+        {\"v\":" . $row['JobRun'] . ",\"f\":null},
+        {\"v\":\"" . $row['Uuid'] . "\",\"f\":null}
         ]}";
         } else {
             echo "{\"c\":[
-        {\"v\":\"" . $row['Uuid'] . "\",\"f\":null},
+        {\"v\":\"" . $row['ExecPath'] . "\",\"f\":null},
         {\"v\":\"" . $row['Date'] . "\",\"f\":null},
         {\"v\":\"" . $row['LinkProgram'] . "\",\"f\":null},
         {\"v\":\"" . $row['ExitCode'] . "\",\"f\":null},
-        {\"v\":" . $row['JobRun'] . ",\"f\":null}
+        {\"v\":\"" . $row['BuildUser'] . "\",\"f\":null},
+        {\"v\":" . $row['JobRun'] . ",\"f\":null},
+        {\"v\":\"" . $row['Uuid'] . "\",\"f\":null}
         ]}, ";
         } 
 
