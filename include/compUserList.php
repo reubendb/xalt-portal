8e33b5269d7676ca468736bafbbbec51b73899fb
kpl-grwl <kapil.agrawal85@gmail.com>
2015-12-12 18:29:06 -0500

    comment history

--- include/compUserList.php
+++ include/compUserList.php
@@ -1,5 +1,7 @@
 <?php
-
+/*
+ * Get list of users for a given linkProgram, sysHost and date range.
+ * */
 $sysHost     = $_GET["sysHost"];
 $startDate   = $_GET["startDate"];
 $endDate     = $_GET["endDate"];
