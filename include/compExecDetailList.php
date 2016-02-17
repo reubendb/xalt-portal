84113ab6ce4fbca06e9810d631c00e4d45e759a4
kpl-grwl <kapil.agrawal85@gmail.com>
2016-02-17 00:25:55 -0500

    pagination changes for most link

--- include/compExecDetailList.php
+++ include/compExecDetailList.php
@@ -9,6 +9,9 @@ $endDate     = $_GET["endDate"];
 $linkProgram = $_GET["linkProgram"];
 $user        = $_GET["user"];
 $exec        = $_GET["exec"];
+$page        = $_GET["page"];
+$rec_limit   = 11;
+$offset      = 0;
 
 try {
     include (__DIR__ ."/wrapper.php");
@@ -16,20 +19,27 @@ try {
 
     $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    if ($linkProgram == 'gpp') { $linkProgram = 'g++'; }              # specail condition for g++ as ajax call reads + as special character
 
-        $sql="
-        SELECT
+    /* specail condition for g++ as ajax call reads + as special character */
+    if ($linkProgram == 'gpp') { 
+        $linkProgram = 'g++'; 
+    }
+
+    /* instead of using offset we need to use rec_limit there is no other way 
+     * to do this with google visualization
+     * */
+
+    if ($page == 0){ $offset = 0; } 
+    else {
+        $rec_limit = 11 * ($page + 1);
+    }
+
+    $sql="SELECT
         xl.exec_path as ExecPath,
         xl.date as BuildDate,
         xl.link_program as LinkProgram,
         xl.exit_code as ExitCode,
         xl.build_user as BuildUser,
-        IF (
-            (SELECT COUNT(*) 
-            FROM xalt_run xr
-            WHERE xr.uuid = xl.uuid) >= 1, 'true', 'false'
-        ) as JobRun,
         xl.uuid as Uuid
         FROM xalt_link xl  
         WHERE xl.build_user = '$user' AND
@@ -37,8 +47,9 @@ try {
         xl.link_program = '$linkProgram' AND
         xl.date BETWEEN '$startDate 00:00:00' AND '$endDate 23:59:59' AND
         SUBSTRING_INDEX(xl.exec_path, '/', -1) = '$exec'
-        ORDER BY Date desc ;";
-
+        ORDER BY Date desc 
+        LIMIT $offset, $rec_limit
+        ;";
 
     $query = $conn->prepare($sql);
     $query->execute();
@@ -63,6 +74,19 @@ try {
         $row_num++;
         $execPath = wrapper($row['ExecPath'], 45);
 
+        /* start ++ check if exec used in a job */
+        $uuid = '';
+        $uuid = $row['Uuid'];
+        $sql = "SELECT IF (
+            (SELECT COUNT(*) FROM 
+            xalt_run WHERE uuid = '$uuid' >= 1), 'true', 'false') 
+            AS JobRun ";
+
+        $q = $conn->prepare($sql);
+        $q->execute();
+        $r = $q->fetchAll(PDO:: FETCH_ASSOC);
+        /* ends -- */
+
         if ($row_num == $total_rows){
             echo "{\"c\":[
         {\"v\":\"" . $execPath . "\",\"f\":null},
@@ -70,7 +94,7 @@ try {
         {\"v\":\"" . $row['LinkProgram'] . "\",\"f\":null},
         {\"v\":\"" . $row['ExitCode'] . "\",\"f\":null},
         {\"v\":\"" . $row['BuildUser'] . "\",\"f\":null},
-        {\"v\":" . $row['JobRun'] . ",\"f\":null},
+        {\"v\":" . $r[0]['JobRun'] . ",\"f\":null},
         {\"v\":\"" . $row['Uuid'] . "\",\"f\":null}
         ]}";
         } else {
@@ -80,7 +104,7 @@ try {
         {\"v\":\"" . $row['LinkProgram'] . "\",\"f\":null},
         {\"v\":\"" . $row['ExitCode'] . "\",\"f\":null},
         {\"v\":\"" . $row['BuildUser'] . "\",\"f\":null},
-        {\"v\":" . $row['JobRun'] . ",\"f\":null},
+        {\"v\":" . $r[0]['JobRun'] . ",\"f\":null},
         {\"v\":\"" . $row['Uuid'] . "\",\"f\":null}
         ]}, ";
         } 
