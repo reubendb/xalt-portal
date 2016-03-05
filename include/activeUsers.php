6aed33db0d5482cc532e7c5e7585a49f3876f868
kpl-grwl <kapil.agrawal85@gmail.com>
2016-03-05 00:49:21 -0500

    fix - data for last year was not showing up in  mysql query

--- include/activeUsers.php
+++ include/activeUsers.php
@@ -3,7 +3,6 @@
  * Get active users based on sysHost and given date range.
  * */
 
-
 $sysHost    = $_GET["sysHost"];
 $startDate  = $_GET["startDate"];
 $endDate    = $_GET["endDate"];
@@ -16,14 +15,13 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $sql = "SELECT MONTH(xr.date) AS Mon_numeric, 
-        MONTHNAME(xr.date) AS Month, 
+        DATE_FORMAT(xr.date, '%b%y') AS Month, 
         YEAR(xr.date) AS Year, 
         COUNT(DISTINCT xr.user) as Users
         FROM xalt_run xr 
         WHERE xr.syshost = '$sysHost' AND 
         xr.date BETWEEN '$startDate 00:00:00' AND '$endDate 23:59:59'
-        GROUP BY Month 
-        HAVING Year = YEAR(curdate())
+        GROUP BY Month, Year
         ORDER BY Year desc, Mon_numeric desc 
         ";
 
@@ -32,23 +30,30 @@ try {
 
     $result = $query->fetchAll(PDO:: FETCH_ASSOC);
 
-    echo "{
-        \"cols\": 
-            [{\"id\":\"\",\"label\":\"Month\",\"pattern\":\"\",\"type\":\"string\"},{\"id\":\"\",\"label\":\"Users\",\"pattern\":\"\",\"type\":\"number\"}], 
-            \"rows\": [ ";
+    echo "{\"cols\": [
+{\"id\":\"\",\"label\":\"Month\",\"pattern\":\"\",\"type\":\"string\"},
+{\"id\":\"\",\"label\":\"Users\",\"pattern\":\"\",\"type\":\"number\"}
+], 
+\"rows\": [ ";
 
-        $total_rows = $query->rowCount();
-        $row_num = 0;
+$total_rows = $query->rowCount();
+$row_num = 0;
 
-        foreach($result as $row){
-            $row_num++;
-            if ($row_num == $total_rows){
-                echo "{\"c\":[{\"v\":\"" . $row['Month'] . "\",\"f\":null},{\"v\":" . $row['Users'] . ",\"f\":null}]}";
-            } else {
-                echo "{\"c\":[{\"v\":\"" . $row['Month'] . "\",\"f\":null},{\"v\":" . $row['Users'] . ",\"f\":null}]}, ";
-            } 
-        }
-        echo " ] }";
+foreach($result as $row){
+    $row_num++;
+    if ($row_num == $total_rows){
+        echo "{\"c\":[
+    {\"v\":\"" . $row['Month'] . "\",\"f\":null},
+    {\"v\":" . $row['Users'] . ",\"f\":null}
+    ]}";
+    } else {
+        echo "{\"c\":[
+    {\"v\":\"" . $row['Month'] . "\",\"f\":null},
+    {\"v\":" . $row['Users'] . ",\"f\":null}
+    ]}, ";
+    } 
+}
+echo " ] }";
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
